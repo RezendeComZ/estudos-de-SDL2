2ea8bd9c5e088969320458482afe872910ec3501
Gabriel Rezende <g@brielrezen.de>
2021-09-02 22:00:48 -0300

    terceira aula do codergopher

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,34 @@
 #include <SDL2/SDL_image.h>
 #include <iostream>
 
-int main(int argv, char* args[])
+#include "RenderWindow.hpp"
+
+int main(int argc, char* args[])
 {
-	std::cout << "Oi" << std::endl;
+	if (SDL_Init(SDL_INIT_VIDEO) > 0)
+		std::cout << "HEY.. SDL_Init HAS FAILED. SDL_ERROR: " << SDL_GetError() << std::endl;
+
+	if (!(IMG_Init(IMG_INIT_PNG)))
+		std::cout << "IMG_init has failed. Error: " << SDL_GetError() << std::endl;
+
+	RenderWindow window("GAME v1.0", 200, 400);
+
+	bool gameRunning = true;
+
+	SDL_Event event;
+
+	while (gameRunning)
+	{
+		// Get our controls and events
+		while (SDL_PollEvent(&event))
+		{
+			if (event.type == SDL_QUIT)
+				gameRunning = false;
+		}
+	}
+
+	window.cleanUp();
+	SDL_Quit();
+
+	return 0;
 }
